75bb6a0e783e8a2aa3e97f4542e61827fd0cd2ef
brendonhps <brendonhps@gmail.com>
2019-09-20 10:59:47 -0300

    application with backend in nodejs by rocketseat

---
+++ NodeJS/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
+const app = express();
+app.use(express.json());
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi');
+
+requireDir('./src/models');
+
+// const Product = mongoose.model('Product');
+
+// Primeira rota
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
